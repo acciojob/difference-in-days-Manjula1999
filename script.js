b987e645b0fb70d98ff2d3371fc51a35c1eaf9e0
Manjula1999 <144819647+Manjula1999@users.noreply.github.com>
2024-09-03 00:03:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var dateDiffInDays = function (date1, date2) {
+var dateDiffInDays = function (dateOne, dateTwo) {
   //   write your code here
 	const oneDay = 24*60*60*1000; // hours in a day * mins in a hour * seconds in a min * mili second in a second
 	const firstDate = new date(dateOne);
-	conts secondDate = new date(dateSecond);
+	conts secondDate = new date(dateTwo);
 
 	const diffIntime = secondDate.getTime() - firstDate.getTime()
 	const diffInDay = Math.round(diffIntime/oneDay);
